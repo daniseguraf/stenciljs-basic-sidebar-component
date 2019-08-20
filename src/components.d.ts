27c85014e676221bae0f1130cda200143971878b
Daniel <danielseguraf@gmail.com>
2019-08-19 23:10:46 -0500

    Creating a New Stencil Web Component

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,53 +9,27 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 
 export namespace Components {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first': string;
-    /**
-    * The last name
-    */
-    'last': string;
-    /**
-    * The middle name
-    */
-    'middle': string;
-  }
+  interface UcSideDrawer {}
 }
 
 declare global {
 
 
-  interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {}
-  var HTMLMyComponentElement: {
-    prototype: HTMLMyComponentElement;
-    new (): HTMLMyComponentElement;
+  interface HTMLUcSideDrawerElement extends Components.UcSideDrawer, HTMLStencilElement {}
+  var HTMLUcSideDrawerElement: {
+    prototype: HTMLUcSideDrawerElement;
+    new (): HTMLUcSideDrawerElement;
   };
   interface HTMLElementTagNameMap {
-    'my-component': HTMLMyComponentElement;
+    'uc-side-drawer': HTMLUcSideDrawerElement;
   }
 }
 
 declare namespace LocalJSX {
-  interface MyComponent extends JSXBase.HTMLAttributes<HTMLMyComponentElement> {
-    /**
-    * The first name
-    */
-    'first'?: string;
-    /**
-    * The last name
-    */
-    'last'?: string;
-    /**
-    * The middle name
-    */
-    'middle'?: string;
-  }
+  interface UcSideDrawer extends JSXBase.HTMLAttributes<HTMLUcSideDrawerElement> {}
 
   interface IntrinsicElements {
-    'my-component': MyComponent;
+    'uc-side-drawer': UcSideDrawer;
   }
 }
 
---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,15 @@
+import { Component, h } from '@stencil/core'
+
+@Component({
+  tag: 'uc-side-drawer'
+})
+
+export class SideDrawer {
+  render() {
+    return (
+      <div>
+        The Side Drawer
+      </div>
+    )
+  }
+}
